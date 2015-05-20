dc9166888e2c176a8f2842348fc4f63ad4a6bb27
Paul Sokolik <paul.sokolik@gmail.com>
2015-05-20 14:37:07 -0700

    'completed taxonomy prototype chains'

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,30 @@
+// an age property that is set to 10
+// a job property that is set to make honey
+// a color property inherited from bee that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a honeyPot property that is set to 0
+// a makeHoney method that adds 1 to that honeyBee\'s honeyPot
+// a giveHoney method that subtracts 1 from that honeyBee\'s honeyPot
+
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  // this.makeHoney = function() {
+  //   this.honeyPot++;
+  // }
+  // this.giveHoney = function() {
+  //   this.honeyPot--;
+  // }
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+    this.honeyPot++;
+};
+HoneyMakerBee.prototype.giveHoney = function() {
+    this.honeyPot--;
+};
